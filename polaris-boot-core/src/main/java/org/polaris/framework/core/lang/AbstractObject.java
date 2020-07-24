cd33181138d2c7d09b3ea69660a8b6e997f69a6a
Nielson Ge <jianfeng.ge@outlook.com>
2020-07-24 13:30:31 +0800

    + add abstract object in core library

---
+++ polaris-boot-core/src/main/java/org/polaris/framework/core/lang/AbstractObject.java
@@ -0,0 +1,13 @@
+package org.polaris.framework.core.lang;
+
+import java.io.Serializable;
+
+/**
+ * Abstract Object
+ *
+ * @author Nielson Ge
+ * @since 0.0.0.1
+ */
+public abstract class AbstractObject extends Object implements Serializable {
+    private static final long serialVersionUID = -4849137638994338197L;
+}
